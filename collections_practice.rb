9ebafea34e305e6e88cb8e775a63c4e2b122ad43
Chloe Gell <gellert.chloe@gmail.com>
2020-01-24 22:14:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(arr)
+  arr.sort
+end 
 
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+    a < b 
+end 
+
+def sort_array_char_count(arr)
+  arr.sort {|a, b| a.length <=> b.length} 
+end 
+
+def swap_elements(arr)
+  
+end 
+
+def reverse_array(arr)
+  arr.reverse 
+end 
+
+def kesha_maker(arr)
+  keshad = []
+  arr.each do |word|
+    keshad.push(word[3] = "$")
+  end
+  return keshad 
+end 
+
+def find_a(arr)
+  arr.select {|word| word.start_with?("a")} 
+end 
+
+def sum_array(arr)
+  arr.inject(0) {|sum, num| sum + num }
+end 
+
+def add_s(arr)
+  arr.each_with_index.collect do |word, idx|
+    if (idx == 1) 
+      word = word 
+    else 
+      word += "s"
+    end 
+  end
+end 
